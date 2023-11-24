22e35c688a66012b528d955d8f5ba67134a2e295
ANSH KAUSHIK <anshkaushik9454@gmail.com>
2023-11-24 20:09:31 +0530

    ui fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,14 @@ import secureVaultLogo from "../assets/homeimage-removebg-preview.png";
 
 function Home() {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 container  ">
+    <div
+      style={{ height: "70vh" }}
+      className="d-flex justify-content-center align-items-center  container  "
+    >
       <div className="row  rounded-5 justify-content-between" style={{ borderRadius: "20px" }}>
         <div className="col-8">
           <div className="card-body">
-            <h2 className="card-title">EMPOWERING BlOCKCHAIN </h2>
+            <h2 className="card-title">EMPOWERING BLOCKCHAIN </h2>
             <h2 className="card-title">STORAGE </h2>
             <p className="card-text my-4">Storing the blockchain world, one block at a Time</p>
             <Link
